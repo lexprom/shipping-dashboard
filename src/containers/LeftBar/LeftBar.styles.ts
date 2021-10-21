58d2cbd5c4b649726fc987303dc30c19fb523df9
lexprom <lexprom009@gmail.com>
2021-10-21 20:45:38 +0400

    Added the LeftBar container

---
+++ src/containers/LeftBar/LeftBar.styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 255px;
+  background: #363740;
+`;
+
+export const AppTitle = styled.div`
+  text-align: center;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 19px;
+  line-height: 24px;
+  letter-spacing: 0.4px;
+  color: #a4a6b3;
+  opacity: 0.7;
+  padding: 41px 43px 43px 43px;
+`;
+
+export const Options = styled.div``;
+
+export const OverviewOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+  background: rgba(159, 162, 180, 0.08);
+  border-left: 3px solid #dde2ff;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  letter-spacing: 0.2px;
+  color: #dde2ff;
+`;
---
+++ src/containers/LeftBar/LeftBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppTitle, Container, Options, OverviewOption } from "./LeftBar.styles";
+// import graphIcon from "assets/bar-chart.png";
+
+const LeftBar = () => {
+  return (
+    <Container>
+      <AppTitle>Dashboard</AppTitle>
+      <Options>
+        <OverviewOption>
+          {/* <img src={graphIcon} /> */}
+          Trade Lanes
+        </OverviewOption>
+      </Options>
+    </Container>
+  );
+};
+
+export default LeftBar;
