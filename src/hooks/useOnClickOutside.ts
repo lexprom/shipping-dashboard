a636dde7ede8be062ba7ddc2ae002357807515cc
lexprom <lexprom009@gmail.com>
2021-10-21 22:55:59 +0400

    Added the custom hook to handle outside clicking

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from "react";
+
+const useOnClickOutside = (
+  ref: RefObject<HTMLElement>,
+  onOutsideClick: () => void,
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onOutsideClick();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+};
+
+export default useOnClickOutside;
