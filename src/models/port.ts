2b4c677a992c4c11c6012e3cd3df7a8c455a46ba
lexprom <lexprom009@gmail.com>
2021-10-21 20:52:06 +0400

    Added two models: Port, MarketRate

---
+++ src/models/port.ts
@@ -0,0 +1,6 @@
+type Port = {
+  code: string;
+  name: string;
+};
+
+export default Port;
---
+++ src/views/TradeLane/Graph/Graph.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Graph.styles";
+
+const Graph = () => {
+  return <Container></Container>;
+};
+
+export default Graph;
