28b44f38f8227da7f8349ecf718ef5c78662fdee
lexprom <lexprom009@gmail.com>
2021-10-22 13:36:31 +0400

    Added the graph component which is displaying the chart and date range

---
+++ src/views/TradeLane/Graph/Graph.styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #ffff;
+  padding: 32px;
+`;
+
+export const PortSelectors = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 18px;
+`;
+
+export const DestinationIcon = styled.img`
+  margin: 0 10px;
+  height: 30px;
+  width: 30px;
+`;
+
+export const ErrorMessage = styled.div`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  letter-spacing: 0.2px;
+  text-align: center;
+`;
--- src/views/TradeLane/Graph/Graph.tsx
+++ src/views/TradeLane/Graph/Graph.tsx
@@ -1,8 +1,77 @@
-import React from "react";
-import { Container } from "./Graph.styles";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import SelectSearch from "src/components/SelectSearch/SelectSearch";
+import { useAppDispatch } from "src/store";
+import {
+  fetchMarketRates,
+  selectErrorMessage,
+  selectFirstPort,
+  selectMarketRates,
+  selectPorts,
+  selectSecondPort,
+  updateFirstPort,
+  updateSecondPort,
+} from "src/store/tradeLane/tradeLaneSlice";
+import {
+  Container,
+  DestinationIcon,
+  ErrorMessage,
+  PortSelectors,
+} from "./Graph.styles";
+import LineChart from "./LineChart";
+import twoArrowsIcon from "../../../../assets/two-arrows.svg";
+import DateRange from "./DateRange";
 
 const Graph = () => {
-  return <Container></Container>;
+  const dispatch = useAppDispatch();
+  const ports = useSelector(selectPorts);
+  const marketRates = useSelector(selectMarketRates);
+  const firstPort = useSelector(selectFirstPort);
+  const secondPort = useSelector(selectSecondPort);
+  const errorMessage = useSelector(selectErrorMessage);
+
+  useEffect(() => {
+    if (firstPort && secondPort) {
+      dispatch(
+        fetchMarketRates({
+          origin: firstPort.code,
+          destination: secondPort.code,
+        }),
+      );
+    }
+  }, [firstPort, secondPort]);
+
+  return (
+    <Container>
+      <PortSelectors>
+        <SelectSearch
+          options={ports}
+          defaultValue={firstPort}
+          onChange={port => dispatch(updateFirstPort(port))}
+        />
+        <DestinationIcon src={twoArrowsIcon} />
+        <SelectSearch
+          options={ports}
+          defaultValue={secondPort}
+          onChange={port => dispatch(updateSecondPort(port))}
+        />
+      </PortSelectors>
+
+      {errorMessage ? (
+        <ErrorMessage>{errorMessage}</ErrorMessage>
+      ) : (
+        <>
+          <LineChart width={600} height={300} marketRates={marketRates} />
+          <DateRange
+            startDate={marketRates.length && marketRates[0].day}
+            endDate={
+              marketRates.length && marketRates[marketRates.length - 1].day
+            }
+          />
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Graph;
